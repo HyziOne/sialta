78c1f4f3484755173a102db277ec5bffc5b44590
HyziOne <72913682+HyziOne@users.noreply.github.com>
2023-02-02 09:02:07 +0100

    login

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -2,6 +2,7 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -19,8 +20,9 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+export const app = initializeApp(firebaseConfig);
+export const analytics = getAnalytics(app);
+export const auth = getAuth(app);
 
 export const db = getFirestore(app);
 // const db = getFirestore(app);
