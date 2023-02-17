771011d08caeb7946d9ffe43b182e541fc8cf871
HyziOne <72913682+HyziOne@users.noreply.github.com>
2023-02-17 11:48:04 +0100

    correction bouton alerte

--- src/components/Calling/CallButton.js
+++ src/components/Calling/CallButton.js
@@ -12,7 +12,7 @@ const CallButton = () => {
   return (
     <TouchableOpacity style={{position: 'absolute', bottom: -350, left: 300,  zIndex: 99999, width: 500, height: 500}} onPress={handlePress}>
       <View style={{backgroundColor: '#EF4124', width: 50, height: 50, borderRadius: 30, alignItems: 'center', display: 'flex', justifyContent: 'center'}}>
-       <FontAwesome5 name={"siren"} color={"#FFF"} size={26} />
+       <FontAwesome5 name={"phone-alt"} color={"#FFF"} size={26} />
       </View>
         
     </TouchableOpacity>
